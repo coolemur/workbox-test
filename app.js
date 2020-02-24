b9ca9f8d4cc126e4aa5e18f25df30c1bad117bb9
coolemur <coolemur@gmail.com>
2020-02-24 15:08:21 +0200

    Dynamic manifest

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+let dynamicManifest = {
+  "name": "test",
+  "short_name": "test",
+  "start_url": window.location.href,
+  "icons": [{
+    "src": `${window.location.href}apple-touch-icon.png`,
+    "sizes": "180x180",
+    "type": "image/png"
+  },
+  {
+    "src": `${window.location.href}android-chrome-512x512.png`,
+    "sizes": "512x512",
+    "type": "image/png"
+  }],
+  "theme_color": "#1F1F1F",
+  "background_color": "#434A7F",
+  "display": "standalone"
+};
+
+const stringManifest = JSON.stringify(dynamicManifest);
+const blob = new Blob([stringManifest], {type: 'application/json'});
+const manifestURL = URL.createObjectURL(blob);
+document.querySelector('[rel="manifest"]').setAttribute('href', manifestURL);
+
 if('serviceWorker' in navigator){
   navigator.serviceWorker.register('sw.js')
     .then(reg => console.info('Service worker registered.'))
